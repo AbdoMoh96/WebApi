e2a38c196623a32421cd4041c1b2c40beabbcece
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2024-04-14 21:54:44 +0200

    working on dtos

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
 
 namespace WebApi.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class StockController : ControllerBase
     {
-        public StockController()
+        private readonly ApplicationDBContext _context;
+
+        public StockController(ApplicationDBContext context)
         {
-            
+            _context = context;
         }
+
+        [HttpGet("GetAllStocks")]
+        public IActionResult GetAll() 
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("GetStockById/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+
+
     }
 }
